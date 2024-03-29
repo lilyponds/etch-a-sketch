881ceb431b7b9439bbb2f362df2e707066d26e2a
tg <157854871+lilyponds@users.noreply.github.com>
2024-03-29 14:29:11 -0500

    Complete TOP Etch-a-sketch challenge, pending Extra Credit.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const container = document.querySelector(".container");
+
+//to start with an initial 16X16 grid
+for(i=0;i<16;i++){
+    const block = document.createElement("div");
+    block.classList.add("block");
+        for(j=0;j<16;j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", ()=>{
+                cell.style.backgroundColor = "purple";
+            });
+          
+            block.appendChild(cell);
+        }
+    container.appendChild(block);
+}
+
 const input = document.querySelector("#size"); //Ask for size of grid
 
 
@@ -9,18 +26,28 @@ const button = document.querySelector("#enter").addEventListener("click",generat
 function generator(){
     count = input.value;
     if(count!=0&&count<=500){
-        container.replaceChildren();
+        container.replaceChildren();//to remove previous grid size
     
-for(i=0;i<count;i++){
-    const block = document.createElement("div"); //to remove previous grid size
-    block.classList.add("block");
-        for(j=0;j<count;j++){
+        for(i=0;i<count;i++){
+        const block = document.createElement("div"); 
+        block.classList.add("block");
+            for(j=0;j<count;j++){
             const cell = document.createElement("div");
             cell.classList.add("cell");
+            //Event listener to create the sketch;
+            cell.addEventListener("mouseenter", ()=>{
+                cell.style.backgroundColor = "purple";
+            });
           
             block.appendChild(cell);
+            }
+            container.appendChild(block);
+            input.value = "";
         }
-    container.appendChild(block);
-    input.value = "";
-}} else {alert("Please enter a valid number that is less than 500 but greater than 0.");}
+    } else {alert("Please enter a valid number that is less than 500 but greater than 0.");}
+
+    const cellColor = document.querySelector(".cell");
+    cellColor.addEventListener("mouseleave", ()=>{
+        cellColor.style.backgroundColor = "purple";
+    });
 }
