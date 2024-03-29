d333e06dc75b147b6978f173064e579786acf5a7
tg <157854871+lilyponds@users.noreply.github.com>
2024-03-29 15:15:16 -0500

    Complete Extra Credit Part 1 - Randomize color and clean up UI.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const input = document.querySelector("#size"); //Ask for size of grid
 
 //Trigger grid creation upon button click
 const button = document.querySelector("#enter").addEventListener("click",generator);
+const clrButton = document.querySelector("#clear").addEventListener("click",starter);
 
 //Function accepts the value from input field to generate that many rows and then each row has that many cells.
 function generator(){
@@ -34,9 +35,13 @@ function generator(){
             for(j=0;j<count;j++){
             const cell = document.createElement("div");
             cell.classList.add("cell");
+            let enterEventCount = 0;
             //Event listener to create the sketch;
             cell.addEventListener("mouseenter", ()=>{
-                cell.style.backgroundColor = "purple";
+                const color = randomColor();
+                console.log(color);
+                cell.style.backgroundColor = color;
+
             });
           
             block.appendChild(cell);
@@ -46,8 +51,31 @@ function generator(){
         }
     } else {alert("Please enter a valid number that is less than 500 but greater than 0.");}
 
-    const cellColor = document.querySelector(".cell");
-    cellColor.addEventListener("mouseleave", ()=>{
-        cellColor.style.backgroundColor = "purple";
-    });
+    
+}
+
+function randomColor(){
+    const r = Math.floor(Math.random()*255+1);
+    const g = Math.floor(Math.random()*255+1);
+    const b = Math.floor(Math.random()*255+1);
+    return 'rgb('+r+','+g+','+b+')';
+}
+
+function starter(){
+    container.replaceChildren();//to remove previous grid 
+    for(i=0;i<16;i++){
+        
+        const block = document.createElement("div");
+        block.classList.add("block");
+            for(j=0;j<16;j++){
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.addEventListener("mouseenter", ()=>{
+                    cell.style.backgroundColor = "purple";
+                });
+              
+                block.appendChild(cell);
+            }
+        container.appendChild(block);
+    }
 }
